4ac23556f55a5699a332c82a21ad95069f7dc165
SARATHKUMAR-T <sarathkumartk98@gmail.com>
2023-05-12 17:46:55 +0530

    formik updation

--- src/Components/UpdateStudents.js
+++ src/Components/UpdateStudents.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Base from "../base/base";
 import { useHistory } from "react-router-dom";
+import { filedValidationSchema} from "./AddStudents";
+import { useFormik } from "formik";
 
 function UpdateStudents({ students, setStudents }) {
   const { id } = useParams();
@@ -19,13 +21,25 @@ function UpdateStudents({ students, setStudents }) {
     setQualification(editStudent.qualification);
   }, [editStudent]);
 
-  async function updateStudent() {
-    const updatedObject = {
-      name: name,
-      batch: batch,
-      gender: gender,
-      qualification: qualification,
-    };
+  const { handleSubmit, values, handleChange, handleBlur, errors, touched } =
+  useFormik({
+    initialValues: { name: "", batch: "", qualification: "", gender: "" },
+
+    validationSchema: filedValidationSchema,
+
+    onSubmit: newStudentData => {
+      console.log("submited", newStudentData);
+      updateStudent(newStudentData);
+    },
+  });
+
+  async function updateStudent(updatedObject) {
+    // const updatedObject = {
+    //   name: name,
+    //   batch: batch,
+    //   gender: gender,
+    //   qualification: qualification,
+    // };
     const response = await fetch(
       `https://644b33c017e2663b9deab94b.mockapi.io/${editStudent.id}`,
       {
@@ -49,59 +63,83 @@ function UpdateStudents({ students, setStudents }) {
   return (
     <Base title={"Edit a Student"} description={"Edit Stuudents data here"}>
       <div className="edit p-4 flex items-center justify-center pr-8  ">
-        <form >
+        <form onSubmit={handleSubmit} >
         <div className="bg-gray-200 flex items-end flex-col rounded-lg shadow-xl p-8 px-16 gap-8 w-94">
-          <div className="flex gap-2">
+          <div className="relative flex gap-2">
             <label for="name" className="pt-2">
               Name:
             </label>
             <input
               className="rounded-md  p-2 outline-none focus:ring-inset duration-300 transition-all focus:ring-[#ff922b] focus:ring-4"
               placeholder="Enter Name"
-              type="text"
+              type="name"
+              name="name"
               id="name"
-              value={name}
-              onChange={e => setName(e.target.value)}
+              onBlur={handleBlur}
+              value={values.name}
+              onChange={handleChange}
             />
+             <div className="-bottom-7 right-4 absolute text-red-600 font-light">
+                {" "}
+                {touched && errors ? errors.name : ""}
+              </div>
           </div>
-          <div className="flex gap-2">
+          <div className="relative flex gap-2">
             <label for="batch" className="pt-2">
               Batch:
             </label>
             <input
               className="rounded-md  p-2 outline-none focus:ring-inset duration-300 transition-all focus:ring-[#ff922b] focus:ring-4"
               placeholder="Enter Batch"
-              type="text"
+              type="batch"
+              name="batch"
               id="batch"
-              value={batch}
-              onChange={e => setBatch(e.target.value)}
+              onBlur={handleBlur}
+              value={values.batch}
+              onChange={handleChange}
             />
+             <div className="-bottom-7 right-4 absolute text-red-600 font-light">
+                {" "}
+                {touched && errors ? errors.batch : ""}
+              </div>
           </div>
-          <div className="flex gap-2">
+          <div className="relative flex gap-2">
             <label for="gender" className="pt-2">
               Gender:
             </label>
             <input
               className="rounded-md  p-2 outline-none focus:ring-inset duration-300 transition-all focus:ring-[#ff922b] focus:ring-4"
               placeholder="Enter Gender"
-              type="text"
+              type="gender"
               id="gender"
-              value={gender}
-              onChange={e => setGender(e.target.value)}
+              name="gender"
+              onBlur={handleBlur}
+              value={values.gender}
+              onChange={handleChange}
             />
+             <div className="-bottom-7 right-4 absolute text-red-600 font-light">
+                {" "}
+                {touched && errors ? errors.gender : ""}
+              </div>
           </div>
-          <div className="flex gap-2">
+          <div className="relative flex gap-2">
             <label for="qualification" className="pt-2">
               Qualification:
             </label>
             <input
               className="rounded-md  p-2 outline-none focus:ring-inset duration-300 transition-all focus:ring-[#ff922b] focus:ring-4"
               placeholder="Enter qualification"
-              type="text"
+              type="qualification"
+              name="qualification"
               id="qualification"
+              onBlur={handleBlur}
               value={qualification}
-              onChange={e => setQualification(e.target.value)}
+              onChange={handleChange}
             />
+             <div className="-bottom-7 right-4 absolute text-red-600 font-light">
+                {" "}
+                {touched && errors ? errors.qualification : ""}
+              </div>
           </div>
 
           <button
